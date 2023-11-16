a36388f47e519b64938fe5cbaae2989fa0b76ea1
Grabaz <fifipolopl@wp.pl>
2023-11-16 17:58:20 +0100

    Dodaj pliki projektów.

---
+++ Lab3/Models/Travel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BookingDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
