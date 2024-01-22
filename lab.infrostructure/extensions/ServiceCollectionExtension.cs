89c21d366206adc39ce1cc881f2546418f04c6c6
Grabaz <fifipolopl@wp.pl>
2024-01-22 20:53:57 +0100

    update

---
+++ lab.infrostructure/extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using lab.infrostructure.persistance;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace lab.infrostructure.extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<Lab3DbContext>(options => options.UseSqlServer(
+                    configuration.GetConnectionString("Airport")));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<Lab3DbContext>();
+        }
+    }
+}
